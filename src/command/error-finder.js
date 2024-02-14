a5621551dcf73fa54b42781cc857e1c14bcc5ee7
Pheonix14 <97115569+Pheonix14@users.noreply.github.com>
2024-02-14 08:07:20 +0000

    Added error-fixer and fixed code-writer

--- src/command/error-finder.js
+++ src/command/error-finder.js
@@ -5,27 +5,38 @@ const embeds = require("./../../config/embeds.json");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("code-writer")
-    .setDescription("Write code for your project")
+    .setName("error-fixer")
+    .setDescription("Find errors in your code and fix it")
     .addStringOption((option) =>
       option
-        .setName("project")
-        .setDescription("project to write code for, eg. discord bot")
+        .setName("error")
+        .setDescription("code's error message, eg. PyroQuanta is not defined")
+        .setRequired(true),
+    )
+    .addStringOption((option) =>
+      option
+        .setName("code")
+        .setDescription("code that have error, eg. console.log(PyroQuanta)")
         .setRequired(true),
     )
     .addStringOption((option) =>
       option
         .setName("coding-lang")
-        .setDescription("programming language, eg. python")
+        .setDescription("programming language, eg. javascript")
         .setRequired(true),
     ),
   async execute(interaction, client) {
-    const project = interaction.options.getString("project");
+    const errorMsg = interaction.options.getString("error");
+    const code = interaction.options.getString("code");
     const codingLang = interaction.options.getString("coding-lang");
 
     try {
       const result = await model.generateContent(
-        `write code for ${project} in ${codingLang} programming language`,
+        `find the error: "${errorMsg}",
+        
+        from the code: "${code}
+        
+        in: "${codingLang} programming language"`,
       );
       const response = await result.response;
       const text = response.text();
--- src/model/gemini-pro.js
+++ src/model/gemini-pro.js
@@ -10,19 +10,19 @@ const genAI = new GoogleGenerativeAI(config.bot.GEMINI_API_KEY);
 const safetySettings = [
   {
     category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-    threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
   },
   {
     category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-    threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
   },
   {
     category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-    threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
   },
   {
     category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-    threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
   },
 ];
 
