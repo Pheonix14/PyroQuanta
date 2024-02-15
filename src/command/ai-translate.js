93bf91e389a411210439ed4f3d8996b132e8647f
Pheonix14 <97115569+Pheonix14@users.noreply.github.com>
2024-02-15 19:20:21 +0000

    Added ai-translate

---
+++ src/command/ai-translate.js
@@ -0,0 +1,54 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const config = require("./../../config/config.json");
+const model = require("./../model/gemini-pro.js");
+const embeds = require("./../../config/embeds.json");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ai-translate")
+    .setDescription("translate text to another language")
+    .addStringOption((option) =>
+      option
+        .setName("from")
+        .setDescription("laguange to translate from, eg. English")
+        .setRequired(true),
+    )
+    .addStringOption((option) =>
+      option
+        .setName("text")
+        .setDescription("text to translate, eg. Hello World")
+        .setRequired(true),
+    )
+    .addStringOption((option) =>
+      option
+        .setName("to")
+        .setDescription("laguange to translate to, eg. Hindi")
+        .setRequired(true),
+    ),
+  async execute(interaction, client) {
+    const fromLang = interaction.options.getString("from");
+    const transText = interaction.options.getString("text");
+    const toLang = interaction.options.getString("to");
+
+    try {
+      const result = await model.generateContent(
+        `translate the text: "${transText}", from: ${fromLang} to: ${toLang}`,
+      );
+      const response = await result.response;
+      const text = response.text();
+
+      const embed = new EmbedBuilder()
+        .setColor(embeds.color)
+        .setTitle(`**PyroQuanta**`)
+        .setDescription(`${text}`)
+        .setFooter({ text: `Powered by Google's Gemini AI` })
+        .setTimestamp();
+
+      await interaction.editReply({ embeds: [embed] });
+    } catch (error) {
+      await interaction.editReply(
+        "Your request is rejected, maybe given language is not supported.",
+      );
+    }
+  },
+};
